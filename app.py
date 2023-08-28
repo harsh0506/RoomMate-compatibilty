a14c1cc7df41e56bf8036d10b9a7e8e06f11ad7f
harsh0506 <joshiharsh0506@gmail.com>
2023-08-28 21:34:37 +0530

    changed routes

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, request, jsonify, send_file ,make_response
+from flask import Flask, request, jsonify, send_file, make_response
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column, Integer, String, Boolean, TIMESTAMP, text
 from datetime import datetime
 import os
 from dotenv import load_dotenv
 import pandas as pd
+import time 
 
 load_dotenv()
 
@@ -43,10 +44,39 @@ class UserData(db.Model):
     deletedat = Column(TIMESTAMP)
 
 
+def data_to_dict(data):
+    return {
+        "id": data.id,
+        "roomsharingprefrence": data.roomsharingprefrence,
+        "yourgender": data.yourgender,
+        "genderpref": data.genderpref,
+        "budget": data.budget,
+        "agerange": data.agerange,
+        "friendshipchoice": data.friendshipchoice,
+        "personalitytrait": data.personalitytrait,
+        "dailyroutine": data.dailyroutine,
+        "roommatesdailyroutine": data.roommatesdailyroutine,
+        "dietrypref": data.dietrypref,
+        "personality": data.personality,
+        "socialstatus": data.socialstatus,
+        "youroccup": data.youroccup,
+        "roommateoccup": data.roommateoccup,
+        "moviepref": data.moviepref,
+        "citipref": data.citipref,
+        "cleanliness": data.cleanliness,
+        "skillscookclean": data.skillscookclean,
+        "comforwithcompany": data.comforwithcompany,
+        "conflictresolution": data.conflictresolution,
+        "boundaries": data.boundaries,
+        "createdat": data.createdat.strftime('%Y-%m-%d %H:%M:%S'),
+        "updatedat": data.updatedat.strftime('%Y-%m-%d %H:%M:%S'),
+        "deletedat": data.deletedat.strftime('%Y-%m-%d %H:%M:%S') if data.deletedat else None
+    }
+
+
 @app.route('/store_data', methods=['POST'])
 def store_data():
-    print(request.get_json())
-    data = request.get_json()  # Assuming JSON data is sent in the request
+    data = request.get_json()
     new_data = UserData(**data)
 
     try:
@@ -66,80 +96,22 @@ def see():
 @app.route('/get_all_data', methods=['GET'])
 def get_all_data():
     try:
+        start_time = time.time()
         all_data = UserData.query.all()
-        data_list = []
+        data_list = [data_to_dict(data) for data in all_data]
+        end_time = time.time()
 
-        for data in all_data:
-            data_dict = {
-                "id": data.id,
-                "roomsharingprefrence": data.roomsharingprefrence,
-                "yourgender": data.yourgender,
-                "genderpref": data.genderpref,
-                "budget": data.budget,
-                "agerange": data.agerange,
-                "friendshipchoice": data.friendshipchoice,
-                "personalitytrait": data.personalitytrait,
-                "dailyroutine": data.dailyroutine,
-                "roommatesdailyroutine": data.roommatesdailyroutine,
-                "dietrypref": data.dietrypref,
-                "personality": data.personality,
-                "socialstatus": data.socialstatus,
-                "youroccup": data.youroccup,
-                "roommateoccup": data.roommateoccup,
-                "moviepref": data.moviepref,
-                "citipref": data.citipref,
-                "cleanliness": data.cleanliness,
-                "skillscookclean": data.skillscookclean,
-                "comforwithcompany": data.comforwithcompany,
-                "conflictresolution": data.conflictresolution,
-                "boundaries": data.boundaries,
-                "createdat": data.createdat.strftime('%Y-%m-%d %H:%M:%S'),
-                "updatedat": data.updatedat.strftime('%Y-%m-%d %H:%M:%S'),
-                "deletedat": data.deletedat.strftime('%Y-%m-%d %H:%M:%S') if data.deletedat else None
-            }
-            data_list.append(data_dict)
+        print(f"Time taken to send  POST requests: {end_time - start_time:.6f} seconds")
 
         return jsonify(data_list), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-
 @app.route('/download_csv', methods=['GET'])
 def download_csv():
     try:
         all_data = UserData.query.all()
-
-        data_list = []
-
-        for data in all_data:
-            data_dict = {
-                "id": data.id,
-                "roomsharingprefrence": data.roomsharingprefrence,
-                "yourgender": data.yourgender,
-                "genderpref": data.genderpref,
-                "budget": data.budget,
-                "agerange": data.agerange,
-                "friendshipchoice": data.friendshipchoice,
-                "personalitytrait": data.personalitytrait,
-                "dailyroutine": data.dailyroutine,
-                "roommatesdailyroutine": data.roommatesdailyroutine,
-                "dietrypref": data.dietrypref,
-                "personality": data.personality,
-                "socialstatus": data.socialstatus,
-                "youroccup": data.youroccup,
-                "roommateoccup": data.roommateoccup,
-                "moviepref": data.moviepref,
-                "citipref": data.citipref,
-                "cleanliness": data.cleanliness,
-                "skillscookclean": data.skillscookclean,
-                "comforwithcompany": data.comforwithcompany,
-                "conflictresolution": data.conflictresolution,
-                "boundaries": data.boundaries,
-                "createdat": data.createdat.strftime('%Y-%m-%d %H:%M:%S'),
-                "updatedat": data.updatedat.strftime('%Y-%m-%d %H:%M:%S'),
-                "deletedat": data.deletedat.strftime('%Y-%m-%d %H:%M:%S') if data.deletedat else None
-            }
-            data_list.append(data_dict)
+        data_list = [data_to_dict(data) for data in all_data]
 
         df = pd.DataFrame(data_list)
         csv_data = df.to_csv(index=False)
